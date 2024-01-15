8058843539bba5dc43bc777da0cb7bf5d0cc7729
oscarbutler <146404666+oscarbutler@users.noreply.github.com>
2024-01-15 22:06:40 +0000

    Make a print statement for user total score

--- run.py
+++ run.py
@@ -48,6 +48,8 @@ def main_game_logic():
         if intro.lower().strip().endswith('hit'):
             user_result_two = user_turn()
             user_total += value(user_result_two)
+            print(f"Your total score is: {user_total}")            
+            
             if user_total > 21:
                 print("You've lost, better luck next time!")
                 return user_total
