66cab55178c555e78ffc182a1476650bbcfa46c1
oscarbutler <146404666+oscarbutler@users.noreply.github.com>
2024-01-15 14:09:43 +0000

    Add explanation to what user function does

--- run.py
+++ run.py
@@ -3,4 +3,9 @@ import random
 cards = [1,2,3,4,5,6,7,8,9,10]
 
 def user():
+    """
+    This will give the user and the opponent their first card
+    and will give the user the chance to get another card or
+    stay with what they have.
+    """
     intro = input("Hit or Stick:\n")
