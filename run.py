3196684d541f691f75a5dc72b39217b6aecbb268
oscarbutler <146404666+oscarbutler@users.noreply.github.com>
2024-01-15 16:17:15 +0000

    Create a user result two

--- run.py
+++ run.py
@@ -17,6 +17,8 @@ jack = 10
 randomised = random.choice(cards)
 user_result_one = print(randomised)
 randomised_two = random.choice(cards)
+user_result_two = random.choice(cards)
+user_result_two = print(randomised_two)
 
 def user():
     """
