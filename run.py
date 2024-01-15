ee684e2cd234d4c7d854842d7ca6cff775f3ea92
oscarbutler <146404666+oscarbutler@users.noreply.github.com>
2024-01-15 22:27:16 +0000

    Change perameter to opponent total score

--- run.py
+++ run.py
@@ -41,7 +41,7 @@ def main_game_logic():
         opponent_score = randomised_two
         opponent_total_score += value(opponent_score)
         print(f"Opponents total score is: {opponent_total_score}")
-        if opponent_score > 21:
+        if opponent_total_score > 21:
            print("You have won. Well done!")
            return user_total, opponent_total_score
     
