9466f9a089b2a374a41fb7412752e33d3c5e4a2f
oscarbutler <146404666+oscarbutler@users.noreply.github.com>
2024-01-15 14:07:24 +0000

    Add cards list

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+cards = [1,2,3,4,5,6,7,8,9,10]
