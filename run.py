e1b8136f9f70c211006897f81c81039c81a4c769
oscarbutler <146404666+oscarbutler@users.noreply.github.com>
2024-01-15 17:29:36 +0000

    Add randomised two variable to opponent function

--- run.py
+++ run.py
@@ -51,6 +51,7 @@ def opponent():
     The score for the opponent which will be automised.
     """
     print("Opponents Turn...\n")
+    randomised_two = random.choice(cards)
     print(randomised_two)
 
 def main():
