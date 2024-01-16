40d69ccbb53d2e987cb462b73f5e956f7dd10661
oscarbutler <146404666+oscarbutler@users.noreply.github.com>
2024-01-16 15:58:07 +0000

    Create an opponent result one variable

--- run.py
+++ run.py
@@ -44,6 +44,7 @@ def main_game_logic():
     user_total = 0
     opponent_total_score = 0
     user_result_one = user_turn()
+    opponent_result_one = opponent_turn()
 
     while True:
         intro = input("Hit or Stick:\n")
