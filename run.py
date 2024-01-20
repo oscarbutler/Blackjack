4dd6d5dbf2af112a217b067c0f1ac488f55224c4
Oscar Butler <oscarbutler>
2024-01-20 16:23:36 +0000

    Add os system in again

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import random
-
+import os
 
 cards = ['2', '3', '4', '5', '6', '7', '8', '9',
          '10', 'ace', 'king', 'queen', 'jack',
@@ -166,9 +166,11 @@ def print_menu():
             continue
 
         if answer == 1:
+            os.system('cls' if os.name == 'nt' else 'clear')
             print("Option 1 has been chosen.")
             game_summarising()
         elif answer == 2:
+            os.system('cls' if os.name == 'nt' else 'clear')
             print(f"""Option 2 has been chosen.\n
               Welcome to a game of 21, you will
               have to get the closest to 21 as possible\n
@@ -178,6 +180,7 @@ def print_menu():
               so be careful!\n
               If the user or opponent goes over 21 then they will lose!\n""")
         elif answer == 3:
+            os.system('cls' if os.name == 'nt' else 'clear')
             print("""Option 3 has been chosen.\n
                 https://github.com/oscarbutler""")
         elif answer == 0:
